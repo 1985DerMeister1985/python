55396e25e8a4425529fb0f6bcb9f9dad30121b30
DerMeister <1985.der.meister.1985@gmail.com>
2023-11-28 11:48:53 +0200

    sync

--- main.py
+++ main.py
@@ -1,7 +1,8 @@
 def my_foo():
     var1 = 5
     var2 = 1
-    var3 = var1+var2
-    return var3
+    var3 = var1<var2
+    if var3 True return var3
+    elif var3 False return var1
 print(my_foo())
     
